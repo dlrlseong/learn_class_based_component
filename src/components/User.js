bf5fb8cbfbdc16ae9d270936d2d567ad86b08bff
dlrlseong <leekisung0212@gmail.com>
2022-08-14 23:39:23 +0900

    174 State 및 이벤트 작업하기

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,6 @@ import classes from "./User.module.css";
 import { Component } from "react";
 
 class User extends Component {
-  
   //render function equals to return function
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
